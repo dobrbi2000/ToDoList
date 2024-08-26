f1d04d7d5acda444bd5611e143b0617eceb6354d
Yauheni Dabrydzeneu <yauhenidabrydzeneu@gmail.com>
2024-08-26 16:23:57 +0300

    init: logon

--- script.js
+++ script.js
@@ -1,6 +1,101 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+//auth
+
+document.addEventListener("DOMContentLoaded", function () {
+  checkUsserAuth();
+});
+
+function checkUsserAuth() {
+  fetch("ya-praktikum.tech/api/v2/auth/user")
+    .then((response) => {
+      if (response.ok) {
+        showToDoApp();
+      } else {
+        showLogonContainer();
+      }
+    })
+    .catch((error) => {
+      console.error("Ошибка при проверке авторизации:", error);
+      showLogonContainer();
+    });
+}
+
+function showLogonContainer() {
+  document.querySelector(".logon-container").style.display = "block";
+  document.querySelector(".todo-app-container").style.display = "none";
+}
+
+function showTodoApp() {
+  document.querySelector(".logon-container").style.display = "none";
+  document.querySelector(".todo-app-container").style.display = "block";
+}
+
+document
+  .getElementById("logonForm")
+  .addEventListener("signupBtn", function (event) {
+    event.preventDefault();
+    const userData = {
+      first_name: document.getElementById("firstName").value,
+      second_name: document.getElementById("secondName").value,
+      login: document.getElementById("login").value,
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value,
+      phone: document.getElementById("phone").value,
+    };
+    createUser(userData);
+  });
+
+function createUser(userData) {
+  fetch("ya-praktikum.tech/api/v2/auth/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("Пользователь успешно создан");
+        showTodoApp;
+      } else {
+        console.log("Ошибка при регистрации, статус: ", response.status);
+      }
+    })
+    .catch((error) => {
+      console.error("Ошибка при создании пользователя:", error);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const signinBtn = document.getElementById("signinBtn");
+  const firstName = document.getElementById("firstName");
+  const secondName = document.getElementById("secondName");
+  const email = document.getElementById("email");
+  const phone = document.getElementById("phone");
+
+  signinBtn.addEventListener("click", function () {
+    firstName.style.maxHeight = "0";
+    secondName.style.maxHeight = "0";
+    email.style.maxHeight = "0";
+    phone.style.maxHeight = "0";
+    title.innerHTML = "Sign In";
+    signupBtn.classList.add("disable");
+    signinBtn.classList.remove("disable");
+  });
+
+  signupBtn.addEventListener("click", function () {
+    firstName.style.maxHeight = "60px";
+    secondName.style.maxHeight = "60px";
+    email.style.maxHeight = "60px";
+    phone.style.maxHeight = "60px";
+    title.innerHTML = "Sign Up";
+    signupBtn.classList.remove("disable");
+    signinBtn.classList.add("disable");
+  });
+});
+
 function addTask() {
   if (inputBox.value === "") {
     alert("You have to write something!");
