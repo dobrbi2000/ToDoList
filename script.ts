dc78e48c4bcccaee05e57829ee603f77bc3b9624
Yauheni Dabrydzeneu <yauhenidabrydzeneu@gmail.com>
2024-10-29 09:17:20 +0300

    vite

--- script.ts
+++ script.ts
@@ -1,4 +1,18 @@
-const app = document.getElementById("app") as HTMLElement;
+import signinPage from './pages/signin.html?raw'; //файл декларация или типы Vite+raw+ts
+import signupPage from './pages/signup.html?raw';
+import todoPage from './pages/todo.html?raw';
+
+//image
+import iconSrc from './images/icon.png';
+
+function setImage() {
+  const imgElement = document.querySelector('.todo-app h2 img') as HTMLImageElement;
+  if (imgElement) {
+    imgElement.src = iconSrc;
+  }
+}
+
+const app = document.getElementById('app') as HTMLElement;
 
 interface RequestParams {
   method: string | undefined;
@@ -11,95 +25,78 @@ interface UserData {
   [key: string]: string;
 }
 
-window.addEventListener("DOMContentLoaded", async () => {
+window.addEventListener('DOMContentLoaded', async () => {
   try {
     const response = await request({
-      method: "GET",
-      suffix: "auth/user",
+      method: 'GET',
+      suffix: 'auth/user',
     });
     if (response) {
       pages.todo();
     } else {
-      throw new Error("Authentication failed");
+      throw new Error('Authentication failed');
     }
   } catch (error) {
     pages.signup();
   }
 });
 
-async function loadPage(pageName: string): Promise<string> {
-  const result: Response = await fetch(`/pages/${pageName}.html`);
-  if (!result.ok) {
-    throw new Error(`Failed to load page: ${pageName}, status: ${result.status}`);
-  }
-  const text = await result.text();
-  return text;
-}
+// async function loadPage(pageName: string): string {
+//   // const result: Response = await fetch(`/pages/${pageName}.html`);
+//   // if (!result.ok) {
+//   //   throw new Error(`Failed to load page: ${pageName}, status: ${result.status}`);
+//   // }
+//   // const text = await result.text();
+//   return pages[pageName];
+// }
 
 const pages = {
-  todo: async () => {
-    try {
-      const text = await loadPage("todo");
-      app.innerHTML = text;
-      updateHistory("todo", "ToDo Page", "/todo");
+  todo: () => {
+    app.innerHTML = todoPage;
+    setImage();
+    updateHistory('todo', 'ToDo Page', '/todo');
 
-      const inputBox = document.getElementById("input-box") as HTMLInputElement;
-      const listContainer = document.getElementById("list-todo-app-container") as HTMLElement;
+    const inputBox = document.getElementById('input-box') as HTMLInputElement;
+    const listContainer = document.getElementById('list-todo-app-container') as HTMLElement;
 
-      setupEventListeners(inputBox, listContainer);
-      showTasks(listContainer);
-    } catch (error) {
-      console.error("Error loading the todo page:", error);
-      app.innerHTML = "<p>Error loading the todo page. Please try again later.</p>";
-    }
+    setupEventListeners(inputBox, listContainer);
+    showTasks(listContainer);
   },
-  signup: async () => {
-    try {
-      const text = await loadPage("signup");
-      app.innerHTML = text;
-      updateHistory("signup", "SignUp Page", "/signup");
-      createNewUser();
-      addClick(".signin-btn", pages.signin);
-    } catch (error) {
-      console.error("Error loading the signup page:", error);
-      app.innerHTML = "<p>Error loading the signup page. Please try again later.</p>";
-    }
+  signup: () => {
+    app.innerHTML = signupPage;
+    updateHistory('signup', 'SignUp Page', '/signup');
+    createNewUser();
+    addClick('.signin-btn', pages.signin);
   },
-  signin: async () => {
-    try {
-      const text = await loadPage("signin");
-      app.innerHTML = text;
-      updateHistory("signin", "SignIn Page", "/signin");
-      loginUser();
-      addClick(".signup-btn", pages.signup);
-    } catch (error) {
-      console.error("Error loading the signin page:", error);
-      app.innerHTML = "<p>Error loading the signin page. Please try again later.</p>";
-    }
+  signin: () => {
+    app.innerHTML = signinPage;
+    updateHistory('signin', 'SignIn Page', '/signin');
+    loginUser();
+    addClick('.signup-btn', pages.signup);
   },
 };
 
-document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener('DOMContentLoaded', async () => {
   const path = window.location.pathname;
-  if (path === "/todo") {
+  if (path === '/todo') {
     pages.todo();
-  } else if (path === "/signup") {
+  } else if (path === '/signup') {
     pages.signup();
-  } else if (path === "/signin") {
+  } else if (path === '/signin') {
     pages.signin();
   } else {
     pages.signup();
   }
 });
 
-window.addEventListener("popstate", (event) => {
+window.addEventListener('popstate', (event) => {
   if (event.state) {
     const page = event.state.page;
-    if (page === "todo") {
+    if (page === 'todo') {
       pages.todo();
-    } else if (page === "signup") {
+    } else if (page === 'signup') {
       pages.signup();
-    } else if (page === "signin") {
+    } else if (page === 'signin') {
       pages.signin();
     }
   } else {
@@ -108,21 +105,21 @@ window.addEventListener("popstate", (event) => {
 });
 
 function createNewUser() {
-  const form = document.getElementById("logonForm") as HTMLElement | null;
+  const form = document.getElementById('logonForm') as HTMLElement | null;
   if (form) {
-    form.addEventListener("submit", async (event: Event) => {
+    form.addEventListener('submit', async (event: Event) => {
       event.preventDefault();
 
       const userData: UserData = {};
-      document.querySelectorAll<HTMLInputElement>("#logonForm input").forEach((input) => {
+      document.querySelectorAll<HTMLInputElement>('#logonForm input').forEach((input) => {
         userData[input.name] = input.value;
       });
 
-      console.log("User information sending to API:", userData);
+      console.log('User information sending to API:', userData);
 
       try {
-        const action = form.getAttribute("action");
-        const method = form.getAttribute("method")?.toUpperCase();
+        const action = form.getAttribute('action');
+        const method = form.getAttribute('method')?.toUpperCase();
         const result = await request({
           suffix: action,
           method: method,
@@ -131,41 +128,41 @@ function createNewUser() {
         if (result) {
           pages.todo();
         } else {
-          console.error("Failed to create user");
+          console.error('Failed to create user');
         }
       } catch (error) {
-        console.error("Request failed:", error);
+        console.error('Request failed:', error);
       }
     });
   } else {
-    console.error("logonForm not found");
+    console.error('logonForm not found');
   }
 }
 
 function addClick(selector: string, action: () => void): void {
   const button = document.querySelector(selector);
   if (button) {
-    button.addEventListener("click", action);
+    button.addEventListener('click', action);
   } else {
     console.error(`Button with selector "${selector}" not found`);
   }
 }
 
 async function loginUser(): Promise<void> {
-  const form = document.getElementById("loginForm") as HTMLHtmlElement | null;
+  const form = document.getElementById('loginForm') as HTMLHtmlElement | null;
   if (form) {
-    form.addEventListener("submit", async (event: Event) => {
+    form.addEventListener('submit', async (event: Event) => {
       event.preventDefault();
 
       const userData: UserData = {};
-      document.querySelectorAll<HTMLInputElement>("#loginForm input").forEach((input) => {
+      document.querySelectorAll<HTMLInputElement>('#loginForm input').forEach((input) => {
         userData[input.name] = input.value;
       });
 
-      console.log("User information sending to API:", userData);
+      console.log('User information sending to API:', userData);
       try {
-        const action = form.getAttribute("action");
-        const method = form.getAttribute("method")?.toUpperCase();
+        const action = form.getAttribute('action');
+        const method = form.getAttribute('method')?.toUpperCase();
         const result = await request({
           suffix: action,
           method: method,
@@ -177,23 +174,23 @@ async function loginUser(): Promise<void> {
           pages.signup();
         }
       } catch (error) {
-        console.error("Error during login:", error);
+        console.error('Error during login:', error);
         pages.signup();
       }
     });
   } else {
-    console.error("loginForm not found");
+    console.error('loginForm not found');
   }
 }
 
-async function request({ method = "POST", suffix, body, credentials = "include" }: RequestParams): Promise<boolean> {
+async function request({ method = 'POST', suffix, body, credentials = 'include' }: RequestParams): Promise<boolean> {
   const headers = {
-    Accept: "application/json",
-    "Content-Type": "application/json",
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
   };
 
   const url = `https://ya-praktikum.tech/api/v2/${suffix}`;
-  console.log("URL API:", url);
+  console.log('URL API:', url);
 
   try {
     const response = await fetch(url, {
@@ -202,49 +199,49 @@ async function request({ method = "POST", suffix, body, credentials = "include" 
       body: JSON.stringify(body),
       credentials: credentials,
     });
-    console.log("Response from Request:", response);
-    const contentType = response.headers.get("content-type");
+    console.log('Response from Request:', response);
+    const contentType = response.headers.get('content-type');
     if (!response.ok) {
-      throw new Error("Request failed with status " + response.status);
+      throw new Error('Request failed with status ' + response.status);
     }
 
-    if (contentType && contentType.includes("application/json")) {
+    if (contentType && contentType.includes('application/json')) {
       await response.json();
     } else {
       await response.text();
     }
     return true;
   } catch (error) {
-    console.error("Request failed:", error);
+    console.error('Request failed:', error);
     return false;
   }
 }
 
 function addTask(inputBox: HTMLInputElement, listContainer: HTMLElement): void {
-  if (inputBox.value === "") {
-    alert("You have to write something!");
+  if (inputBox.value === '') {
+    alert('You have to write something!');
   } else {
-    let li = document.createElement("li");
+    let li = document.createElement('li');
     li.textContent = inputBox.value;
     listContainer.appendChild(li);
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
+    let span = document.createElement('span');
+    span.innerHTML = '\u00d7';
     li.appendChild(span);
   }
-  inputBox.value = "";
+  inputBox.value = '';
   saveData(listContainer);
 }
 
 function handleEvent(event: Event, inputBox: HTMLInputElement, listContainer: HTMLElement) {
   const { type, target, key } = event as KeyboardEvent & MouseEvent;
-  if (type === "keydown" && key === "Enter") {
+  if (type === 'keydown' && key === 'Enter') {
     event.preventDefault();
     addTask(inputBox, listContainer);
   }
-  if (type === "click" && target instanceof HTMLElement) {
-    if (target.tagName === "LI") {
-      target.classList.toggle("checked");
-    } else if (target.tagName === "SPAN" && target.parentElement) {
+  if (type === 'click' && target instanceof HTMLElement) {
+    if (target.tagName === 'LI') {
+      target.classList.toggle('checked');
+    } else if (target.tagName === 'SPAN' && target.parentElement) {
       target.parentElement.remove();
     }
   }
@@ -253,24 +250,24 @@ function handleEvent(event: Event, inputBox: HTMLInputElement, listContainer: HT
 }
 
 function setupEventListeners(inputBox: HTMLInputElement, listContainer: HTMLElement): void {
-  const addButton = document.getElementById("add-task-btn");
+  const addButton = document.getElementById('add-task-btn');
   if (addButton instanceof HTMLButtonElement) {
-    addButton.addEventListener("click", () => addTask(inputBox, listContainer));
+    addButton.addEventListener('click', () => addTask(inputBox, listContainer));
   } else {
-    console.error("Add button not found");
+    console.error('Add button not found');
   }
 
-  listContainer.addEventListener("click", (event: MouseEvent) => handleEvent(event, inputBox, listContainer));
-  inputBox.addEventListener("keydown", (event: KeyboardEvent) => handleEvent(event, inputBox, listContainer));
+  listContainer.addEventListener('click', (event: MouseEvent) => handleEvent(event, inputBox, listContainer));
+  inputBox.addEventListener('keydown', (event: KeyboardEvent) => handleEvent(event, inputBox, listContainer));
 }
 
 function saveData(listContainer: HTMLElement): void {
-  localStorage.setItem("data", listContainer.innerHTML);
+  localStorage.setItem('data', listContainer.innerHTML);
 }
 
 function showTasks(listContainer: HTMLElement): void {
-  const storeData = localStorage.getItem("data");
-  listContainer.innerHTML = storeData !== null ? storeData : "";
+  const storeData = localStorage.getItem('data');
+  listContainer.innerHTML = storeData !== null ? storeData : '';
 }
 
 function updateHistory(pageName: string, pageTitle: string, pagePath: string) {
